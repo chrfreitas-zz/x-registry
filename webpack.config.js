3ffeedb8269e45c9186d4de84f335bf3411d8608
Christian Freitas <chrfreitas@gmail.com>
2017-12-20 21:42:08 -0200

    Add uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
 const config = {
     entry: './src/js/App.js',
     output: {
@@ -7,15 +9,18 @@ const config = {
     module: {
         loaders: [
             {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['react', 'es2015']
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
             }
-}
         ]
-    }
+    },
+    plugins: [
+        new UglifyJsPlugin()
+    ]
 };
 
 module.exports = config;
